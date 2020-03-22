0f675a5e252ca4923507d1196bfe596480a78a64
Ben Mohammad <benmohammad593@gmail.com>
2020-03-22 17:13:11 +0000

    mergeSort added...

--- app/src/main/java/com/benmohammad/datastructuresandalgos/MainActivity.java
+++ app/src/main/java/com/benmohammad/datastructuresandalgos/MainActivity.java
@@ -16,8 +16,14 @@ public class MainActivity extends AppCompatActivity {
         int[] intArray = {20, 35, -15, 7, 55, 1, -22};
 
 
-        Log.v(TAG, iterativeFactorial(3) + "");
-        Log.v(TAG, recursiveFactorial(3) + "");
+        mergeSort(intArray, 0, intArray.length);
+
+        for(int i = 0; i< intArray.length; i++) {
+            Log.v(TAG, intArray[i]+ "");
+        }
+
+        //Log.v(TAG, iterativeFactorial(3) + "");
+        //Log.v(TAG, recursiveFactorial(3) + "");
 
         //shellSort(intArray);
         //insertionSort(intArray);
@@ -28,6 +34,36 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    }
+
+    private void mergeSort(int[] input, int start, int end) {
+        if(end - start < 2) {
+            return;
+        }
+
+        int mid = (start + end) / 2;
+        mergeSort(input, start, mid);
+        mergeSort(input, mid, end);
+        merge(input, start, mid, end);
+
+    }
+
+    private void merge(int[] input, int start, int mid, int end) {
+        if(input[mid - 1] < input[mid]) {
+            return;
+        }
+
+        int i = start;
+        int j = mid;
+        int tempIndex = 0;
+
+        int[] temp = new int[end - start];
+        while(i < mid && j < end) {
+            temp[tempIndex++] = input[i] <= input[j] ? input[i++] : input[j++];
+        }
+
+        System.arraycopy(input, i, input, start + tempIndex, mid - i);
+        System.arraycopy(temp, 0, input, start, tempIndex);
     }
 
     private void shellSort(int[] input) {
